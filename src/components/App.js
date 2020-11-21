8a2331e10b2461c81453eaf1be29fac9ce529c3c
Yana_Laurynovich <Super_mario_02>
2020-11-21 18:53:13 +0300

    task 1

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../App.css';
+import Header from './Header.js'
+
+const App = React.createElement('div',
+    {
+        className: 'App'
+    },
+    <Header/>)
+
+export default App
